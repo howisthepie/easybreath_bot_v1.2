e8f01b0a3cdc16a3995b05193175750e46b6d4b9
Bogdan Mishalov <bogdanmissss@gmail.com>
2025-07-21 23:41:33 +0300

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,6 @@
+conf = {
+    BOT_TOKEN = '7755502455:AAE_IJSovKqsc67zc04q_3-QnTE5TJfduoc'
+    OPENROUTER_API_KEY = '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'
+    OPENROUTER_URL = 'https://openrouter.ai/api/v1/chat/completions'
+    DB_NAME = 'databese.db'
+}
